663f0af6573d052cd472838016e35c48974fedaa
saikoushik_addanki <saikoushik_addanki@epam.com>
2021-05-03 10:35:58 +0530

    Implemented eureka client in cloud gateway service

--- spring-cloud-api-gateway/src/main/java/com/koushik/apigateway/ApigatewaySampleApplication.java
+++ spring-cloud-api-gateway/src/main/java/com/koushik/apigateway/ApigatewaySampleApplication.java
@@ -3,10 +3,12 @@ package com.koushik.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableDiscoveryClient
 @EnableFeignClients(basePackages= {"com.koushik.apigateway.configuration", "com.koushik.apigateway.filters", "com.koushik.apigateway.service"})
 public class ApigatewaySampleApplication {
 
